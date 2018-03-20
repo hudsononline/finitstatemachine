c854755c5f467c4fc91c9cfe5eafe4a0bbb83aab
Qingyun Xiu <admin@Qingyuns-MacBook-Pro.local>
2018-03-20 10:57:52 -0400

    First commit

--- FiniteStateMachine/ViewController.swift
+++ FiniteStateMachine/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import SwiftState
+
 
 class ViewController: UIViewController {
-
+    
+    enum MyState: StateType {
+        case locked, unlocked
+    }
+    
+    enum MyEvent: EventType {
+        case coin, push
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // setup state machine
+        let machine = StateMachine<MyState, MyEvent>(state: .locked) { machine in
+            
+            // add locked => unlocked => unlocked
+            machine.addRoutes(event: .coin, transitions: [
+                .locked => .unlocked,
+                .unlocked => .unlocked,
+                ])
+            
+            // add unlocked => locked => locked
+            machine.addRoutes(event: .push, transitions: [
+                .unlocked => .locked,
+                .locked => .locked,
+                ])
+
+            // add event handler
+            machine.addHandler(event: .coin) { context in
+                if (context.fromState == MyState.locked) {
+                    print("=> unlocks the turnstile!")
+                }
+            }
+            // add event handler
+            machine.addHandler(event: .push) { context in
+                if (context.fromState == MyState.unlocked) {
+                    print("=> locks the turnstile!")
+                }
+            }
+        }
+        print("initial state: \(machine.state)")
+        print("put a coint")
+        machine <-! .coin
+        print("push the arm")
+        machine <-! .push
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
---
+++ Pods/Target Support Files/Pods-FiniteStateMachineTests/Pods-FiniteStateMachineTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FiniteStateMachineTests : NSObject
+@end
+@implementation PodsDummy_Pods_FiniteStateMachineTests
+@end
